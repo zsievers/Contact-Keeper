d52bc3db9e700c98ff0d1b6ad25a52cdb4287e3f
Zach <zjsievers@gmail.com>
2020-08-28 12:59:17 -0400

    add and delete contacts only from ui

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -42,9 +42,14 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //  add contact
-
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   //  delete contact
-
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   //  set current contact
 
   //  Clear current contact
@@ -56,7 +61,13 @@ const ContactState = (props) => {
   //  clear filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        addContact,
+        deleteContact,
+      }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
